516096b57d2e0ba29f7bdf9a12c24b116d0170aa
jeff <jfgiraud@gmail.com>
2014-11-17 23:01:06 +0100

    ajout

---
+++ src/main/java/BookServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BookServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println( "SimpleServlet Executed" );
+        out.flush();
+        out.close();
+    }
+
+}
